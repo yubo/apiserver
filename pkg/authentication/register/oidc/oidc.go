3243db03dd704e7215274e4fd94ad21067234554
yubo <yubo@yubo.org>
2021-07-03 05:32:03 -0400

    doc: add oidc flag description

--- pkg/authentication/register/oidc/oidc.go
+++ pkg/authentication/register/oidc/oidc.go
@@ -4,13 +4,10 @@ import (
 	"context"
 	"fmt"
 
-	"github.com/spf13/pflag"
 	"github.com/yubo/apiserver/pkg/authentication"
 	"github.com/yubo/apiserver/pkg/authentication/token/oidc"
 	"github.com/yubo/apiserver/pkg/options"
 	"github.com/yubo/golib/proc"
-	cliflag "github.com/yubo/golib/staging/cli/flag"
-	"github.com/yubo/golib/util"
 	"k8s.io/klog/v2"
 )
 
@@ -28,69 +25,26 @@ var (
 		Priority:    proc.PRI_SYS_INIT,
 		SubPriority: options.PRI_M_AUTHN - 1,
 	}}
-	_config *config
 )
 
 type config struct {
-	CAFile         string
-	ClientID       string
-	IssuerURL      string
-	UsernameClaim  string
-	UsernamePrefix string
-	GroupsClaim    string
-	GroupsPrefix   string
-	SigningAlgs    []string
-	RequiredClaims map[string]string
-}
+	CAFile string `json:"caFile" flag:"oidc-ca-file" description:"If set, the OpenID server's certificate will be verified by one of the authorities in the oidc-ca-file, otherwise the host's root CA set will be used."`
 
-func (o *config) addFlags(fs *pflag.FlagSet) {
-	fs.StringVar(&o.IssuerURL, "oidc-issuer-url", o.IssuerURL, ""+
-		"The URL of the OpenID issuer, only HTTPS scheme will be accepted. "+
-		"If set, it will be used to verify the OIDC JSON Web Token (JWT).")
+	ClientID string `json:"clientID" flag:"oidc-client-id" description:"The client ID for the OpenID Connect client, must be set if oidc-issuer-url is set."`
 
-	fs.StringVar(&o.ClientID, "oidc-client-id", o.ClientID,
-		"The client ID for the OpenID Connect client, must be set if oidc-issuer-url is set.")
+	IssuerURL string `json:"issuerURL" flag:"oidc-issuer-url" description:"The URL of the OpenID issuer, only HTTPS scheme will be accepted. If set, it will be used to verify the OIDC JSON Web Token (JWT)."`
 
-	fs.StringVar(&o.CAFile, "oidc-ca-file", o.CAFile, ""+
-		"If set, the OpenID server's certificate will be verified by one of the authorities "+
-		"in the oidc-ca-file, otherwise the host's root CA set will be used.")
+	UsernameClaim string `json:"usernameClaim" flag:"oidc-username-claim" description:"The OpenID claim to use as the user name. Note that claims other than the default ('sub') is not guaranteed to be unique and immutable. This flag is experimental, please see the authentication documentation for further details."`
 
-	fs.StringVar(&o.UsernameClaim, "oidc-username-claim", "sub", ""+
-		"The OpenID claim to use as the user name. Note that claims other than the default ('sub') "+
-		"is not guaranteed to be unique and immutable. This flag is experimental, please see "+
-		"the authentication documentation for further details.")
+	UsernamePrefix string `json:"usernamePrefix" flag:"oidc-username-prefix" description:"If provided, all usernames will be prefixed with this value. If not provided, username claims other than 'email' are prefixed by the issuer URL to avoid clashes. To skip any prefixing, provide the value '-'."`
 
-	fs.StringVar(&o.UsernamePrefix, "oidc-username-prefix", "", ""+
-		"If provided, all usernames will be prefixed with this value. If not provided, "+
-		"username claims other than 'email' are prefixed by the issuer URL to avoid "+
-		"clashes. To skip any prefixing, provide the value '-'.")
+	GroupsClaim string `json:"groupsClaim" flag:"oidc-groups-claim" description:"If provided, the name of a custom OpenID Connect claim for specifying user groups. The claim value is expected to be a string or array of strings. This flag is experimental, please see the authentication documentation for further details."`
 
-	fs.StringVar(&o.GroupsClaim, "oidc-groups-claim", "", ""+
-		"If provided, the name of a custom OpenID Connect claim for specifying user groups. "+
-		"The claim value is expected to be a string or array of strings. This flag is experimental, "+
-		"please see the authentication documentation for further details.")
+	GroupsPrefix string `json:"groupsPrefix" flag:"oidc-groups-prefix" description:"If provided, all groups will be prefixed with this value to prevent conflicts with other authentication strategies."`
 
-	fs.StringVar(&o.GroupsPrefix, "oidc-groups-prefix", "", ""+
-		"If provided, all groups will be prefixed with this value to prevent conflicts with "+
-		"other authentication strategies.")
+	SigningAlgs []string `json:"signingAlgs" default:"RS256" flag:"oidc-signing-algs" description:"Comma-separated list of allowed JOSE asymmetric signing algorithms. JWTs with a 'alg' header value not in this list will be rejected. Values are defined by RFC 7518 https://tools.ietf.org/html/rfc7518#section-3.1."`
 
-	fs.StringSliceVar(&o.SigningAlgs, "oidc-signing-algs", []string{"RS256"}, ""+
-		"Comma-separated list of allowed JOSE asymmetric signing algorithms. JWTs with a "+
-		"'alg' header value not in this list will be rejected. "+
-		"Values are defined by RFC 7518 https://tools.ietf.org/html/rfc7518#section-3.1.")
-
-	fs.Var(cliflag.NewMapStringStringNoSplit(&o.RequiredClaims), "oidc-required-claim", ""+
-		"A key=value pair that describes a required claim in the ID Token. "+
-		"If set, the claim is verified to be present in the ID Token with a matching value. "+
-		"Repeat this flag to specify multiple claims.")
-
-}
-
-func (o *config) changed() interface{} {
-	if o == nil {
-		return nil
-	}
-	return util.Diff2Map(defaultConfig(), o)
+	RequiredClaims map[string]string `json:"requiredClaims" flag:"oidc-required-claim" description:"A key=value pair that describes a required claim in the ID Token. If set, the claim is verified to be present in the ID Token with a matching value. Repeat this flag to specify multiple claims."`
 }
 
 func (o *config) Validate() error {
@@ -119,14 +73,14 @@ type authModule struct {
 	config *config
 }
 
-func defaultConfig() *config {
+func newConfig() *config {
 	return &config{}
 }
 
 func (p *authModule) init(ctx context.Context) error {
 	c := proc.ConfigerFrom(ctx)
 
-	cf := defaultConfig()
+	cf := newConfig()
 	if err := c.Read(p.name, cf); err != nil {
 		return err
 	}
@@ -157,6 +111,5 @@ func (p *authModule) init(ctx context.Context) error {
 
 func init() {
 	proc.RegisterHooks(hookOps)
-	_config = defaultConfig()
-	_config.addFlags(proc.NamedFlagSets().FlagSet("authentication"))
+	proc.RegisterFlags(moduleName, "authentication", newConfig())
 }
