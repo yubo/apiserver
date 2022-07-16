da23de0e42cb8fca56133c25986056980ddb2c4f
yubo <yubo@yubo.org>
2022-07-16 14:04:34 +0800

    add gorelease with examples/all-in-one

---
+++ examples/all-in-one/scripts/env.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+ROOT=`cd $(dirname $0)/../; pwd`
+cd ${ROOT}
+
+source "${ROOT}/scripts/version.sh"
+version::get_version_vars
+
+GOLDFLAGS="${GOLDFLAGS=-s -w} $(version::ldflags)"
+
+cat << EOF
+GOLDFLAGS="$GOLDFLAGS"
+CGO_ENABLED=1
+EOF
